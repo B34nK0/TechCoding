3cd77ffba091c3c76ee4e7c9ba09c01243bc7dbf
BBinchina <185392361@qq.com>
2021-11-24 23:50:28 +0800

    1update

---
+++ question/quickSort.hpp
@@ -0,0 +1,16 @@
+#include <vector>
+
+class quickSort {
+public:
+    static void sortNum(std::vector<int> nums, int l, int r) {
+        int index = (r - l) / 2; 
+    }
+
+    static std::vector<int> sort(std::vector<int>& nums) {
+        if (nums.size() < 2) {
+            return nums;
+        }
+
+        sortNum(nums, 0, nums.size());
+    }
+}
