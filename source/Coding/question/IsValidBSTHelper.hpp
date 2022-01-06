c9fd7721cb76d53790c1c219a3abf0746de46e2e
B34nK0 <185392361@qq.com>
2022-01-06 23:32:06 +0800

    判断树是否为二叉查找树

---
+++ source/Coding/question/IsValidBSTHelper.hpp
@@ -0,0 +1,32 @@
+/*
+判断树是否为二叉查找树
+*/
+
+struct TreeNode {
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+
+	TreeNode(int x) :val(x), left(nullptr), right(nullptr) {
+
+	}
+};
+
+class IsValidBSTHelper {
+public :
+	bool IsValidBST(TreeNode* root) {
+		return isValid(root, nullptr, nullptr);
+	}
+
+	bool isValid(TreeNode* root, TreeNode* leftChild, TreeNode* rightChild) {
+		
+		if (!root) return true;
+
+		//左子树值大于n节点 或者 右子树值大于n节点
+		if ((leftChild && leftChild->val >= root->val) || (rightChild && rightChild->val <= root->val)) {
+			return false;
+		}
+
+		return isValid(root->left, leftChild, root) && isValid(root->right, root, rightChild);
+	}
+};
