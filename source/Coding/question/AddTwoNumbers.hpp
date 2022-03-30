9e83ae30973801d78785dc0d3c06c972985de19b
B34nK0 <185392361@qq.com>
2022-03-30 23:23:50 +0800

    leetCode2 两个链表数值相加

--- source/Coding/question/AddTwoNumbers.hpp
+++ source/Coding/question/AddTwoNumbers.hpp
@@ -1,12 +1,11 @@
-#pragma once
+/*
+leetCode 2两数相加
+*/
 
 #include "CommonStruct.h"
-/// <summary>
-/// leetCode 21 合并两个升序链表
-/// </summary>
-class MergeTwoLists {
+class AddTwoNumbers {
 public:
-	static ListNode* GetMergeRes(ListNode* listA, ListNode* listB) {
+	static ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
 		if (l1 == nullptr) {
 			return l2;
 		}
@@ -17,24 +16,24 @@ public:
 			return nullptr;
 		}
 		//先制造空节点用于指向要返回的结果链表头
-		ListNode ans, * tail = &ans;
+		ListNode pre;
+		ListNode* head = pre.next;
 
 		//当前的进位值
 		int curIncr = 0;
 		//遍历两个链表
 		int curValue = 0;
 		while (l1 != nullptr || l2 != nullptr) {
-			curValue = 0;
 			if (l1 == nullptr) {
-				curValue = l2->val + curIncr;
+				curValue = l2->val;
 				l2 = l2->next;
 			}
 			else if (l2 == nullptr) {
-				curValue = l1->val + curIncr;
+				curValue = l1->val;
 				l1 = l1->next;
 			}
 			else {
-				curValue = l1->val + l2->val + curIncr;
+				curValue = l1->val + l2->val;
 				l1 = l1->next;
 				l2 = l2->next;
 			}
@@ -43,15 +42,15 @@ public:
 			curValue = curValue % 10;
 
 			ListNode* n = new ListNode(curValue);
-			tail->next = n;
-			tail = tail->next;
+			head->next = n;
+			head = head->next;
 		}
 
-		if (curIncr > 0) {
-			ListNode* n = new ListNode(curIncr);
-			tail->next = n;
+		if (curValue > 0) {
+			ListNode* n = new ListNode(curValue);
+			head->next = n;
 		}
 
-		return ans.next;
+		return pre.next;
 	}
 };
