64001421823c358dd7ef7b5be1bcc2f221605dbb
B34nK0 <185392361@qq.com>
2022-04-05 23:19:43 +0800

    leetCode 215 数组中的第K个最大元素
    利用小根堆，维持k个元素，即栈顶值为第k大元素

--- source/Coding/question/FindKthLargest.hpp
+++ source/Coding/question/FindKthLargest.hpp
@@ -1,15 +1,34 @@
 #pragma once
 #include <vector>
 #include <time.h>
-
+#include <queue>
 
 /*
+* leetCode 215. 数组中的第K个最大元素
 给定整数数组 nums 和整数 k，请返回数组中第 k 个最大的元素。
 */
 
 using namespace std;
 class FindKthLargest {
 public:
+
+	/*
+	做法一：建立小根堆（栈顶值时堆中最小的），经过k-1次排序后，栈顶的值即是第k个最大元素
+	//1、可以借用优先队列
+	2、自己建立堆
+	*/
+	int byPriorityQueue(vector<int>& nums, int k) {
+		priority_queue<int, vector<int>, greater<int>> minHeap(nums.begin(), nums.begin() + k);
+		for (size_t i = k; k < nums.size(); ++i) {
+			//当前值大于栈顶值时，说明栈顶一定不是k个最大元素里的一个
+			if (nums[i] > minHeap.top()) {
+				minHeap.pop();
+				minHeap.push(nums[i]);
+			}
+		}
+		return minHeap.top();
+	}
+
 	int quickSelect(vector<int>& nums, int left, int right, int k) {
 		//查找轴值的位置
 		int kindex = findKindex(nums, left, right);
